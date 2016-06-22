344d95b7d07998c0467178ac89313f0c1aa65acc
fayou147 <fayou147@163.com>
2016-06-22 11:46:23 +0800

    test merge

--- app/src/main/java/com/fy/administrator/jnitoso/MainActivity.java
+++ app/src/main/java/com/fy/administrator/jnitoso/MainActivity.java
@@ -28,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        //merge test
         util.destory();
     }
 
